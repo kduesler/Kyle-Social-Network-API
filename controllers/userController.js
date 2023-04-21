392dd9e5cc26f1f29d59e126d819a6cced413fb8
kduesler <kyle.duesler.09@gmail.com>
2023-04-21 02:55:26 -0500

    working deleteFriend

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  // Get a single user
+  
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
       .populate("thoughts")
@@ -20,7 +20,7 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
-  // create a new user
+
   createUser(req, res) {
     User.create(req.body)
       .then((user) => res.json(user))
@@ -40,7 +40,7 @@ module.exports = {
       })
       .catch((err) => res.status(500).json(err));
   },
-  // Delete a user and associated apps
+
   deleteUser(req, res) {
     User.findOneAndDelete({ _id: req.params.userId })
       .then((user) =>
@@ -68,37 +68,18 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 
-  // deleteFriend(req, res) {
-  //   User.findOneAndUpdate(
-  //     { _id: req.params.userId },
-  //     { $set: req.body },
-  //     { runValidators: true, new: true }
-  //   )
-  //     .then((user) => {
-  //       !user
-  //         ? res.status(404).json({ message: "No user with that ID" })
-  //         : res.json(user);
-  //     })
-  //     .catch((err) => res.status(500).json(err));
-  // },
-
   deleteFriend(req, res) {
-    User.findOneAndRemove({ _id: req.params.friendId })
-      .then((friend) =>
-        !friend
-          ? res.status(404).json({ message: 'No friend with this id!' })
-          : User.findOneAndUpdate(
-              { friends: req.params.friendId },
-              { $pull: { friends: req.params.friendId } },
-              { new: true }
-            )
-      )
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } },
+      { new: true }
+    )
       .then((user) =>
         !user
           ? res.status(404).json({
-              message: 'No user with this id!',
+              message: "No user with this id!",
             })
-          : res.json({ message: 'Friend successfully deleted!' })
+          : res.json({ message: "Friend successfully deleted!" })
       )
       .catch((err) => res.status(500).json(err));
   },
